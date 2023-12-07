2faac2cdd7d3e3094b75c5b1168bdea82abf4592
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2023-12-06 13:38:13 -1200

    my chrome extention

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function(){
+    console.log("button clicked")
+})  */
+
+
+let myLeads = []
+let inputEl = document.createElementById("input-el")
+let inpuBtn = document.createElementById("input-btn")
+
+
+
+
+let inputBtn = document.getElementsById("input-btn")
+inputBtn.addEventListener("click", function(){
+console.log("Button clicked")
+})
+
+
